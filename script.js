04cf2d59cda7b149e12e961ef34fdc393e00aeaf
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-17 15:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = [];
+for (let khiladi of players){
+	team1.push(khiladi);
+}
+let cap1 = {...person}
+
+
+	
+
 
 
 export {players,person,team,team1,cap1}
